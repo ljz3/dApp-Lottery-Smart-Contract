1ade79f044dc5d847afae6e6d6d4aadf1717a1ec
ljz3 <kevin.ottawa@hotmail.com>
2021-09-15 16:25:55 -0400

    Changed config to deploy to Rinkeby Testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,10 +12,17 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
-
+const INFURA_API_KEY = "";
+const RINKEBY_PRIVATE_KEY = "";
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [`0x${RINKEBY_PRIVATE_KEY}`],
+    },
+  },
 };
